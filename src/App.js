ba4d531c5b9a6f2867cf2f2ae253614d0dfffec1
Deebakkumar <deebakkumar6682@gmail.com>
2024-02-18 18:07:24 +0530

    Add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import{BrowserRouter,Route,Routes} from 'react-router-dom'
+import Adminlogin from './Components/Admin/Adminlogin';
+import Adminpage from './Components/Admin/Adminpage';
+import Addproduct from './Components/Admin/Addproduct';
+import Products from './Components/Productpage/Products';
+import Landingpage from './Components/Landingpage/Landingpage';
+import { useState } from 'react';
+import EditProduct from './Components/Admin/EditProduct';
+import Cart from './Components/Cart/Cart';
+import Contactus from './Components/Contactus/Contactus';
+import Usersdetails from './Components/Admin/Usersdetails';
 
 function App() {
+  const[user,setuser]=useState("");
+  const[cart,setcart]=useState("");
+  const userdata=(item)=>{
+        setuser(item)
+  }
+
+  const addCart=(item)=>{
+        setcart(item)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+    <Route exact 
+      path='/' 
+      element={<Landingpage/>} >
+      </Route>
+    <Route exact 
+      path='/admin/login' 
+      element={<Adminlogin/>} >
+      </Route>
+      <Route exact 
+      path='admin/adminpage' 
+      element={<Adminpage userdata={userdata}/>} >
+      </Route>
+      <Route exact 
+      path='admin/addproduct' 
+      element={<Addproduct/>} >
+      </Route>
+      <Route exact 
+      path='/products' 
+      element={<Products addCart={addCart}/>} >
+      </Route>
+      <Route exact 
+      path='admin/editproduct' 
+      element={<EditProduct user={user}/>} >
+      </Route>
+      <Route exact 
+      path='/cart' 
+      element={<Cart cart ={cart}/>} >
+      </Route>
+      <Route exact 
+      path='/contactus' 
+      element={<Contactus/>} >
+      </Route>
+      <Route exact 
+      path='/admin/userdetails' 
+      element={<Usersdetails/>} >
+      </Route>
+      </Routes>
+      </BrowserRouter>
+
   );
 }
 
